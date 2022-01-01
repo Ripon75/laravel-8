c82cd7344e8954ee9158f9bb24e501f4cafa06a2
Ripon75 <riponahmedjrtr@gmail.com>
2022-01-01 22:26:27 +0600

    Add multiple image

--- app/Http/Controllers/Admin/ImageController.php
+++ app/Http/Controllers/Admin/ImageController.php
@@ -9,11 +9,10 @@ use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.z
-     *
-     * @return \Illuminate\Http\Response
-     */
+   public function __construct() {
+       $this->middleware('auth');
+   }
+   
     public function index()
     {
         $images = MultiImage::all();
